06f5ed73b6abf672a1da8ea252805a3c302e14d0
Giancarlo Solís <gsoliscastillo@gmail.com>
2023-11-13 15:55:41 -0500

    changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.hosts << "cfa9-38-25-34-122.ngrok-free.app"
-  config.web_console.permissions = '38.25.34.122'
+  # config.web_console.permissions = '38.25.34.122'
 end
