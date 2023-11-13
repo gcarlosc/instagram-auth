94528f0e9504cefc48afd9613e4f24ee9ff64a95
Giancarlo Solís <gsoliscastillo@gmail.com>
2023-11-13 17:22:37 -0500

    change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
+  # config.action_controller.raise_on_missing_callback_actions = true
   config.hosts << "cfa9-38-25-34-122.ngrok-free.app"
   # config.web_console.permissions = '38.25.34.122'
 end
